7d13a8f95abf55fc902f67c3e63207bf0d21f51e
Jiri Hnidek <jiri.hnidek@tul.cz>
2013-12-08 22:24:45 +0100

    Added empty callback functions.

--- verse_client.c
+++ verse_client.c
@@ -5,6 +5,30 @@
 /* My session ID */
 static uint8_t my_session_id = -1;
 
+/* My callback function for user authentication */
+void cb_receive_user_authenticate(const uint8_t session_id,
+        const char *username,
+        const uint8_t auth_methods_count,
+        const uint8_t *methods)
+{
+    /* TODO: Do something useful here */
+}
+
+/* My callback function for connect accept */
+void cb_receive_connect_accept(const uint8_t session_id,
+        const uint16_t user_id,
+        const uint32_t avatar_id)
+{
+    /* TODO: Do something useful here */
+}
+
+/* My callback function for connect terminate */
+void cb_receive_connect_terminate(const uint8_t session_id,
+        const uint8_t error_num)
+{
+    /* TODO: Do something useful here */
+}
+
 int main(void)
 {
     int error_num;
