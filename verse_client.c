94c269080e679d6472e870f59e9daeb26653617c
Jiri Hnidek <jiri.hnidek@tul.cz>
2013-12-08 22:50:26 +0100

    Added logic to callback functions, fixed errors and refactoring.

--- verse_client.c
+++ verse_client.c
@@ -1,12 +1,14 @@
 #include <verse.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 /* My session ID */
-static uint8_t my_session_id = -1;
+static uint8_t my_session_id = 255;
 
 /* Error value */
-static int error_num;
+static int error;
 
 /* My callback function for user authentication */
 void cb_receive_user_authenticate(const uint8_t session_id,
@@ -14,7 +16,20 @@ void cb_receive_user_authenticate(const uint8_t session_id,
         const uint8_t auth_methods_count,
         const uint8_t *methods)
 {
-    /* TODO: Do something useful here */
+    char name[64], *password;
+
+    /* Get username, when it is requested */
+    if(username == NULL) {
+        printf("Username: ");
+        scanf("%s", name);
+        vrs_send_user_authenticate(session_id, name, 0, NULL);
+    } else {
+        /* We assume that VRS_UA_METHOD_PASSWORD is in methods array */
+        strcpy(name, username);
+        /* Get password from user */
+        password = getpass("Password: ");
+        vrs_send_user_authenticate(session_id, name, VRS_UA_METHOD_PASSWORD, password);
+    }
 }
 
 /* My callback function for connect accept */
@@ -22,14 +37,21 @@ void cb_receive_connect_accept(const uint8_t session_id,
         const uint16_t user_id,
         const uint32_t avatar_id)
 {
-    /* TODO: Do something useful here */
+    /* When we are connected, then terminate connection. */
+    printf("Connection to Verse server established. Terminating this connection.\n");
+    vrs_send_connect_terminate(my_session_id);
 }
 
 /* My callback function for connect terminate */
 void cb_receive_connect_terminate(const uint8_t session_id,
         const uint8_t error_num)
 {
-    /* TODO: Do something useful here */
+    /* Connection to Verse server was terminated */
+    printf("Connection to Verse server terminated.\n");
+    my_session_id = 255;
+    if(!(error_num == VRS_CONN_TERM_CLIENT || error_num == VRS_CONN_TERM_SERVER)) {
+        error = VRS_FAILURE;
+    }
 }
 
 int main(void)
@@ -40,18 +62,17 @@ int main(void)
     vrs_register_receive_connect_terminate(cb_receive_connect_terminate);
 
     /* Send connect request to the server */
-    error_num = vrs_send_connect_request("localhost", "12345", 0, &my_session_id);
+    error = vrs_send_connect_request("localhost", "12345", 0, &my_session_id);
 
-    /* Was*/
-    if(error_num == VRS_SUCCESS) {
+    if(error == VRS_SUCCESS) {
         /* Never ending loop */
-        while(1) {
+        while(my_session_id != 255) {
             vrs_callback_update(my_session_id);
             sleep(1);
         }
     }
 
-    if(error_num != VRS_SUCCESS) {
+    if(error != VRS_SUCCESS) {
         return EXIT_FAILURE;
     }
 
